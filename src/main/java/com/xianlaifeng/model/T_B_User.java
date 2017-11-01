df28362d64ab3a7f9d3552f5dfa0c38587d94709
chenyi <717572168@qq.com>
2017-11-01 22:55:06 +0800

    frist commit

---
+++ src/main/java/com/xianlaifeng/model/T_B_User.java
@@ -0,0 +1,19 @@
+package com.xianlaifeng.model;
+
+public class T_B_User {
+
+    private String id;
+    private String user_id; //openid
+    private String user_name; //用户名
+    private int user_sex;  //
+    private int user_role; //用户角色（企业发布，兼职学生）
+    private String user_company; //用户公司（企业用户）
+    private String user_birthday;
+    private int user_high;
+    private String user_school_id; //用户学校
+    private String user_qq;   //用户QQ
+    private int user_phone;  //用户电话
+    private String user_details; //用户简历
+
+
+}
