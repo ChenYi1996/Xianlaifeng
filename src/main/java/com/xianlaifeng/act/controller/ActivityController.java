cac18b369668d31b83ef35d35d0db9e669069255
chenyi <717572168@qq.com>
2017-11-27 20:02:41 +0800

    活动模块增加完善（用户报名）

--- src/main/java/com/xianlaifeng/act/controller/ActivityController.java
+++ src/main/java/com/xianlaifeng/act/controller/ActivityController.java
@@ -124,7 +124,7 @@ public class ActivityController {
     }
 
 
-    //活动报名接口
+    //用户活动报名接口
     @RequestMapping(value="/joinAct.do",produces="application/json" ,method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
     public AjaxJSON joinActivity(@RequestParam Map<String,Object> params,@RequestBody AjaxJSON ajax){
